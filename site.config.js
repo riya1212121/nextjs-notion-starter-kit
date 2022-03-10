2b51e8af148d5e0939c7c5723a55da044e175ac4
riya1212121 <100967123+riya1212121@users.noreply.github.com>
2022-03-11 00:44:54 +0530

    initial configs

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'f13d9c3a65a34a31a824ca7607bbeb1c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Riya ❤️',
+  domain: 'xtop.digitalinfolink.in',
+  author: 'Riya',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'This website is created to share timepass only. I have no serrious intention and evrything I\'m sharing is already available on the internet 👍',
+  socialImageTitle: 'xTop Riya',
+  socialImageSubtitle: 'Let\' watch',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
